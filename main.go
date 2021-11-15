9023a0316fef6b3a4a3ee2c08ba2d4b093cd8861
waketakuo <takuowake@gmail.com>
2021-11-15 21:18:06 +0900

    change main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package web_app_go
 
 import (
 	"fmt"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
@@ -32,5 +34,19 @@ func runServer(args arguments) error {
 		return fmt.Errorf("invalid log level: %s", args.LogLevel)
 	}
 	logger.SetLevel(level)
+	logger.SetFormatter(&logrus.JSONFormatter{})
 
+	logger.WithFields(logrus.Fields{
+		"args": args,
+	}).Info("Given options")
+
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile(args.StaticContents, false)))
+	r.GET("/api/v1/hello", func(c *gin.Context) {
+		c.String(200, `{"message": "hello, hello, hello"}`)
+	})
+	if err := r.Run(fmt.Sprintf("%s:%d", args.BindAddress, args.BindPort)); err != nil {
+		return err
+	}
+	return nil
 }
